e1995002b77d6281f47aa06e027d9ed88bcaba51
JohnErikcObando <Jeobando11@hotmail.com>
2021-08-14 09:39:17 -0500

    Registrarse

--- src/app/components/inicio/login/login.component.ts
+++ src/app/components/inicio/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from '../../../models/usuario';
 
 @Component({
   selector: 'app-login',
@@ -20,6 +21,14 @@ export class LoginComponent implements OnInit {
   log() {
 
     console.log(this.login);
+
+    const usuario: Usuario = {
+      nombreUsuario: this.login.value.usuario,
+      password: this.login.value.password
+    }
+
+    console.log(usuario);
+
 
 
   }
